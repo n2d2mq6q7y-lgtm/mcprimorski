7c0218a01d071f907e07bbcecc51f7b729967ce2
Arthrex23 <n2d2mq6q7y@privaterelay.appleid.com>
2026-01-03 09:45:36 +0200

    Update settings.py

--- clinic_site/clinic_site/settings.py
+++ clinic_site/clinic_site/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = []
 
 MEDIA_URL = '/media/'
